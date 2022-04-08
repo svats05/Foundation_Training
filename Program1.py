f8050c129b195b0fae652b5e6e95989b0aa75b3e
Shubham Vats <shubhamvats3099@gmail.com>
2022-04-08 12:55:38 +0530

    Update Program1.py

--- Program1.py
+++ Program1.py
@@ -1,7 +1,7 @@
 def fact(n):
-    c=1
-    for i in range(1,n+1):
-        c=c*i
-    return c
+    if n == 0:
+        return 1
+    return n * fact(n - 1)
+
 inp=int(input("Enter the number: "))
 print(fact(inp))
